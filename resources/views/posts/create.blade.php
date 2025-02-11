c6ed0112ef7c298148ea98ef424fcc2a0abf09f0
Gina González <gini_go@hotmail.com>
2025-02-11 18:43:05 +0100

    create view to create post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,7 @@
-<h1>Hola Laravel</h1>
+<form action="{{route('posts.new')}}" method="post">
+    <label for="image">Add Image</label>
+    <input type="text" name="image" id="image">
+    <label for="caption">Add a description</label>
+    <textarea name="caption" id="caption"></textarea>
+    <input class="bg-red-50" type="submit" value="Publish">
+</form>
